9bd0449e9cdbdb29cb9cbe34d9c515c59c566ca4
root <root@1b8b8c1d3998>
2024-07-01 11:16:53 +0000

    user_fakes

--- download.py
+++ download.py
@@ -2,13 +2,13 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/training')
+os.chdir('/content/vidpro')
 import shutil
 shutil.make_archive('HEINZO', 'zip', '/content/images')
-full_local_path = "/content/training"
+full_local_path = "/content/vidpro"
 
-backupname = "/content/training/HEINZO.zip"
-backup = ('/content/training/H' + str(tgl) + '.zip')
+backupname = "/content/vidpro/HEINZO.zip"
+backup = ('/content/vidpro/H' + str(tgl) + '.zip')
 os.rename(backupname, backup)
 
 
--- training.py
+++ training.py
@@ -2,9 +2,9 @@ import os
 from git import Repo
 import datetime as dt
 tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/training')
+os.chdir('/content/vidpro')
 
-full_local_path = "/content/training"
+full_local_path = "/content/vidpro"
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
